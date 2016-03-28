65a20a974192a046d83a9115d9ec6a8edc2e5819
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-03-28 18:55:14 +0200

    Enable splash

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'uoc-notifier', 'pascalprecht.translate'])
 
@@ -10,7 +11,7 @@ angular.module('starter', ['ionic', 'uoc-notifier', 'pascalprecht.translate'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
