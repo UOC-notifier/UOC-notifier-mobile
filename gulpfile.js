d1263004c9bbb4bf2454f76950a7dc9504002a78
Pau Ferrer Ocaña <crazyserver@gmail.com>
2017-01-28 20:28:56 +0100

    Clean

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var minifyCss = require('gulp-minify-css');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var del = require('del');
 
 var license = '' +
   '// Copyright (C) 2017 Pau Ferrer Ocaña\n' +
@@ -41,6 +42,7 @@ var paths = {
 
 gulp.task('default', ['sass', 'js']);
 gulp.task('build', ['sass', 'js']);
+gulp.task('build-prepare', ['build']);
 
 gulp.task('sass', function(done) {
   gulp.src(paths.sass)
@@ -95,4 +97,50 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('delete-files', function() {
+ //remove unnecessary files
+ console.log('delete-files STARTED');
+ del([
+   'platforms/android/assets/www/controllers/**',
+   'platforms/android/assets/www/css/**',
+   'platforms/android/assets/www/directives/**',
+   'platforms/android/assets/www/main.js',
+   'platforms/android/assets/www/lib/ionic/**',
+   '!platforms/android/assets/www/lib/ionic',
+   '!platforms/android/assets/www/lib/ionic/release',
+   '!platforms/android/assets/www/lib/ionic/release/js',
+   '!platforms/android/assets/www/lib/ionic/release/js/ionic.bundle.min.js',
+   '!platforms/android/assets/www/lib/ionic/release/fonts',
+   '!platforms/android/assets/www/lib/ionic/release/fonts/**',
+   'platforms/android/assets/www/lib/chart.js/**',
+   '!platforms/android/assets/www/lib/chart.js',
+   '!platforms/android/assets/www/lib/chart.js/dist',
+   '!platforms/android/assets/www/lib/chart.js/dist/Chart.min.js',
+   'platforms/android/assets/www/lib/angular-chart.js/**',
+   '!platforms/android/assets/www/lib/angular-chart.js',
+   '!platforms/android/assets/www/lib/angular-chart.js/dist',
+   '!platforms/android/assets/www/lib/angular-chart.js/dist/angular-chart.min.js',
+   'platforms/android/assets/www/lib/angular-animate/**',
+   'platforms/android/assets/www/lib/angular/**',
+   'platforms/android/assets/www/lib/angular-sanitize/**',
+   'platforms/android/assets/www/lib/angular-ui-router/**',
+   'platforms/android/assets/www/lib/angular-translate/**',
+   '!platforms/android/assets/www/lib/angular-translate',
+   '!platforms/android/assets/www/lib/angular-translate/angular-translate.min.js',
+   'platforms/android/assets/www/lib/angular-translate-loader-static-files/**',
+   '!platforms/android/assets/www/lib/angular-translate-loader-static-files',
+   '!platforms/android/assets/www/lib/angular-translate-loader-static-files/angular-translate-loader-static-files.min.js',
+   'platforms/android/assets/www/lib/ngCordova/**',
+   '!platforms/android/assets/www/lib/ngCordova',
+   '!platforms/android/assets/www/lib/ngCordova/dist',
+   '!platforms/android/assets/www/lib/ngCordova/dist/ng-cordova.min.js',
+   'platforms/android/assets/www/lib/platform.js/**',
+   '!platforms/android/assets/www/lib/platform.js',
+   '!platforms/android/assets/www/lib/platform.js/platform.js'
+   ])
+   .then(function() {
+     console.log('delete-files DONE');
+   });
 });
